d262308ed9d9f0d88ee1b4b1d60a7e4d920568d0
Mino <k3000js@naver.com>
2021-03-10 10:35:20 +0900

    Module 추가 - Network / Repository / Source / ViewModel

--- app/src/main/java/com/mino/assignment/di/NetworkModule.kt
+++ app/src/main/java/com/mino/assignment/di/NetworkModule.kt
@@ -1,7 +1,38 @@
 package com.mino.assignment.di
 
+import com.mino.assignment.data.source.remote.network.BookApi
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
+import retrofit2.CallAdapter
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
+    single<BookApi> {
+        Retrofit.Builder()
+            .addConverterFactory(get())
+            .addCallAdapterFactory(get())
+            .client(get())
+            .baseUrl(BookApi.BASE_URL)
+            .build()
+            .create(BookApi::class.java)
+    }
 
+    single<CallAdapter.Factory> { RxJava2CallAdapterFactory.create() }
+
+    single<Converter.Factory> { GsonConverterFactory.create() }
+
+    factory {
+        OkHttpClient.Builder()
+            .addInterceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .addHeader("Authorization", "KakaoAK ${BookApi.REST_API_KEY}")
+                    .build()
+                val response = it.proceed(request)
+                response
+            }.build()
+    }
 }
--- app/src/main/java/com/mino/assignment/di/SourceModule.kt
+++ app/src/main/java/com/mino/assignment/di/SourceModule.kt
@@ -1,7 +1,9 @@
 package com.mino.assignment.di
 
+import com.mino.assignment.data.source.remote.BookRemoteDataSource
+import com.mino.assignment.data.source.remote.BookRemoteDataSourceImpl
 import org.koin.dsl.module
 
 val sourceModule = module {
-
+        single<BookRemoteDataSource> { BookRemoteDataSourceImpl(get()) }
 }
