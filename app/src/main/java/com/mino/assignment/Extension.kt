e6d6a8ff24061d9b48eee9c438479458ce72e595
Mino <k3000js@naver.com>
2021-03-10 10:38:17 +0900

    Extension 추가

---
+++ app/src/main/java/com/mino/assignment/Extension.kt
@@ -0,0 +1,28 @@
+package com.mino.assignment
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
+    add(disposable)
+}
+
+
+fun <T> Single<T>.networkSchedulers(): Single<T> =
+    subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+fun <T> Observable<T>.networkSchedulers(): Observable<T> =
+    subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+inline fun FragmentManager.transact(action: FragmentTransaction.() -> Unit) {
+    beginTransaction().apply {
+        action()
+    }.commit()
+}
