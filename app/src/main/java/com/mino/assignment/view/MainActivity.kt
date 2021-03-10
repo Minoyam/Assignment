73c0cf8773bca6cbec58d4cbff40c004fd406681
Mino <k3000js@naver.com>
2021-03-10 10:41:16 +0900

    MainActivity 수정

---
+++ app/src/main/java/com/mino/assignment/view/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.mino.assignment.view
+
+import android.os.Bundle
+import com.mino.assignment.R
+import com.mino.assignment.basic.BaseActivity
+import com.mino.assignment.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main,R.id.fl_container) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startFragment(BookListFragment.newInstance(),true)
+    }
+}
